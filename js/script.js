f053527b2f65089aafb099aa484e60ff60611dfe
Elizabeth1401 <140805762+Elizabeth1401@users.noreply.github.com>
2024-11-15 13:23:48 +0100

    add corect code

--- js/script.js
+++ js/script.js
@@ -6,29 +6,35 @@ $("#wateringcan").on("click", function() {
 let waterDropInterval;
 $("#wateringcan").on("click", function(){
     if($(this).hasClass("tilted")){
-        waterDropInterval = setInterval(createWaterDrop, 500);
+        waterDropInterval = setInterval( createWaterDrop, 700);
     }else{
         clearInterval(waterDropInterval);
     }
 });
 
 function createWaterDrop(){
-    let $waterDrop = $("<div class='waterdrop'></div>");
-    $(window).addend($waterDrop);
+    let $waterDrop = $("<div class='waterdrop'><img src='images/waterdrop.png' alt='water drop'></div>");
+    $("body").append($waterDrop);
+    $waterDrop.find("img").css({
+        width: "20px",  
+        height: "40px"  
+    });
     $waterDrop.css({
+        position: 'absolute',
         left: $("#wateringcan").position().left +10,
-        top: $("#wateringcan").position().top +20
+        top: $("#wateringcan").position().top  +90
+        
     });
     $waterDrop.animate({
         top: $(window).height()
-    }, 2000, function(){
+    }, 2000 , function(){
          $(this).remove();
     });
 }
 
-$(document).mousemove(function(e) {
-    $("#follow").css({
-        left: e.pageX,
-        top: e.pageY
-    });
-});
+// $(document).mousemove(function(e) {
+//     $("#follow").css({
+//         left: e.pageX,
+//         top: e.pageY
+//     });
+// });
