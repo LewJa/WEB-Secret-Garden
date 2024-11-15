9382665dc8ea4e7e384fc9a30ff23362680dc8d5
Teo <teodora.cristiana.stoicescu@gmail.com>
2024-11-15 13:54:57 +0100

    small changes

--- js/script.js
+++ js/script.js
@@ -1,40 +1,40 @@
 //MAKE THE MAGIC HAPPEN
-$("#wateringcan").on("click", function() {
+$("#wateringcan").on("click", function () {
     $(this).toggleClass("tilted");
 });
 
 let waterDropInterval;
-$("#wateringcan").on("click", function(){
-    if($(this).hasClass("tilted")){
-        waterDropInterval = setInterval( createWaterDrop, 700);
-    }else{
+$("#wateringcan").on("click", function () {
+    if ($(this).hasClass("tilted")) {
+        waterDropInterval = setInterval(createWaterDrop, 700);
+    } else {
         clearInterval(waterDropInterval);
     }
 });
 
-function createWaterDrop(){
+function createWaterDrop() {
     let $waterDrop = $("<div class='waterdrop'><img src='images/waterdrop.png' alt='water drop'></div>");
     $("body").append($waterDrop);
     $waterDrop.find("img").css({
-        width: "20px",  
-        height: "40px"  
+        width: "20px",
+        height: "40px"
     });
     $waterDrop.css({
         position: 'absolute',
-        left: $("#wateringcan").position().left +10,
-        top: $("#wateringcan").position().top  +90
-        
+        left: $("#wateringcan").position().left + 10,
+        top: $("#wateringcan").position().top + 90
+
     });
     $waterDrop.animate({
         top: $(window).height()
-    }, 2000 , function(){
-         $(this).remove();
+    }, 2000, function () {
+        $(this).remove();
     });
 }
 
- $(document).mousemove(function(e) {
-     $("#follow").offset({
-         left: e.pageX,
-         top: e.pageY
-     });
- });
+$(document).mousemove(function (e) {
+    $("#follow").offset({
+        left: e.pageX,
+        top: e.pageY
+    });
+});
